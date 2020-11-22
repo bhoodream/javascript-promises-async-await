c5302aff71559c2db37961d3f91b99e4d8251722
Vadim <bhoodream@yandex.ru>
2020-11-22 22:41:32 +0700

    module9

--- src/index.js
+++ src/index.js
@@ -79,3 +79,13 @@ getBooksOrMoviesAsync().then((results) => {
         results,
     });
 });
+
+const timer1 = setTimeout(() => {
+    console.log('timer 1 has finished');
+}, 3000);
+
+const timer2 = setTimeout(() => {
+    console.log('timer 2 has finished');
+
+    clearTimeout(timer1);
+}, 2000);
